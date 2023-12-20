e78b1e8c21c28e3fac1c6bcf24e0bce2fdfb71cd
coosdog <153588371+coosdog@users.noreply.github.com>
2023-12-20 21:32:00 +0900

    몬스터 시작카메라 추가

---
+++ Assets/RDS/Script/MoneStartScene.cs
@@ -0,0 +1,27 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Temp;
+using UnityEngine;
+
+public class MoneStartScene : MonoBehaviour
+{
+     CinemachineDollyCart dolly;
+     CinemachineVirtualCamera vCam;
+    public Monster monmon;
+
+    private void Start()
+    {
+        dolly = GetComponent<CinemachineDollyCart>();
+        vCam = GetComponent<CinemachineVirtualCamera>();
+    }
+    private void Update()
+    {
+        if(dolly.m_Position > 86)
+        {
+            vCam.Priority = 0;
+            monmon.GetComponent<Monster>().enabled = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
