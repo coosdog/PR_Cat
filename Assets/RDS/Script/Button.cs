0f5e69343869926d62c68b4efe27b1fe0ba45555
coosdog <153588371+coosdog@users.noreply.github.com>
2023-12-21 17:19:47 +0900

    돌맵 수정2

--- Assets/RDS/Script/Button.cs
+++ Assets/RDS/Script/Button.cs
@@ -3,32 +3,92 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using JongWoo;
+using System;
+
+public abstract class ButtonMode
+{
+    protected Button owner;
+    public ButtonMode(Button owner)
+    {
+        this.owner = owner;
+    }
+    public abstract void Function();
+}
+public class StartButton : ButtonMode
+{
+    public StartButton(Button owner) : base(owner)
+    { }
+    public override void Function()
+    {
+        if (owner.isCreate == false)
+        {
+            Debug.Log("ÀÛµ¿");
+            for (int i = 0; i < owner.Maker.Length; i++)
+            {
+                owner.Maker[i].StartMake();
+                //Maker[i].gameObject.SetActive(false);
+            }
+                owner.isCreate = true;
+        }
+    }
+}
+public class EndButton : ButtonMode
+{
+    public EndButton(Button owner) : base(owner)
+    { }
+    public override void Function()
+    {
+        for (int i = 0; i < owner.Maker.Length; i++)
+        {
+            owner.Maker[i].CancelInvoke();
+        }
+    }
+}
+
+[Flags]
+public enum ButtonType
+{
+    Start = 1 << 0,
+    End = 1 << 1
+}
 public class Button : MonoBehaviour
 {
     public CreateCar[] Maker = new CreateCar[2];
-    
+    public ButtonType buttonType;
+    public List<ButtonMode> buttons = null;
 
+    public bool isCreate = false;
+
+    bool isCheckType(ButtonType type)
+    {
+        return (buttonType & type) != 0;
+    }
     private void Start()
     {
+        buttons = new List<ButtonMode>();
+        if (isCheckType(ButtonType.Start))
+            buttons.Add(new StartButton(this));
+        if (isCheckType(ButtonType.End))
+            buttons.Add(new EndButton(this));
         //SC.audioSource.loop = true;
+    }
+    public void Action()
+    {
+        foreach (ButtonMode b in buttons)
+        {
+            b.Function();
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<Temp.TestPlayer>() != null)
+
+        if (other.GetComponent<Temp.TestPlayer>() != null)
         {
-            Debug.Log("µé¾î¿È");
-            for(int i = 0; i < Maker.Length; i++) 
-            {
-                Maker[i].CancelInvoke();
-                //Maker[i].gameObject.SetActive(false);
-            }
+            Action();
         }
-        if(other.GetComponent<Bounce>() != null)
+        if (other.GetComponent<Bounce>() != null)
         {
-            for (int i = 0; i < Maker.Length; i++)
-            {
-                Maker[i].CancelInvoke();
-            }
+            Action();
         }
     }
     /*
