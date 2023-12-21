71c07aef3fd150f59c8eb88fc59cfcf0cee01512
YUNHOO123 <141704115+YUNHOO123@users.noreply.github.com>
2023-12-22 02:58:04 +0900

    플레이어 카메라 버그 수정
    버그 수정완

--- Assets/RDS/Script/TrashCamera.cs
+++ Assets/RDS/Script/TrashCamera.cs
@@ -3,22 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Photon.Pun;
 
-public class TrashCamera : MonoBehaviour
+public class TrashCamera : MonoBehaviourPun
 {
-
     public VariableJoystick joystick;
 
     public CinemachineFreeLook cam;
     // Start is called before the first frame update
     void Start()
     {
-        cam = GetComponent<CinemachineFreeLook>();
+        if (photonView.IsMine)
+        {
+            cam = GetComponent<CinemachineFreeLook>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!photonView.IsMine)
+            return;
+
         cam.m_YAxis.m_InputAxisValue = -joystick.Vertical;//Input.GetAxis("Vertical");
         cam.m_XAxis.m_InputAxisValue = -joystick.Horizontal;// Input.GetAxis("Horizontal");
     }
