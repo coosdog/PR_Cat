f45e47bf6728f4850565562e8c2ec72bc030659a
khkg12 <78714947+khkg12@users.noreply.github.com>
2024-01-02 21:36:15 +0900

    게임 승패조건 추가

--- Assets/YHDH/GameManager.cs
+++ Assets/YHDH/GameManager.cs
@@ -12,23 +12,39 @@ public class GameManager : Singleton<GameManager>
 {
     public event Action onGameStart;
     public event Action onGameEnd;      
+    public int PlayerCount
+    {
+        get => playerCount;
+        set
+        {
+            playerCount = value;            
+            if(playerCount == 1)
+            {
+                // ½Â¸®¾ÀÀüÈ¯
+                Debug.Log("½Â¸®");
+            }
+        }
+    }
+    private int playerCount;
 
     private void Start()
     {
         //if (!photonView.IsMine)
         //    return;        
-        SceneManager.sceneLoaded += (Scene scene, LoadSceneMode lsm) => { GameStart(); };        
-    }
-    
-
-
+        onGameStart += () => { PlayerCount = PhotonNetwork.CurrentRoom.PlayerCount; };
+        onGameEnd += () => { PlayerCount = 0; }; 
+        SceneManager.sceneLoaded += (Scene scene, LoadSceneMode lsm) => 
+        {
+            if (scene.name == "Lobby" || scene.name == "WaitRoom")
+                return;
+            GameStart(); 
+        };        
+    }    
 
     public void GameStart()
     {
-
         onGameStart?.Invoke();
     }
-
     public void GameEnd()
     {
         onGameEnd?.Invoke();
